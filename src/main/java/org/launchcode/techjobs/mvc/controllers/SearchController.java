99b1cfd7108b51b09e7dd113cc0e6c05c43cabe8
Melissa Matos <melissamomtoast@gmail.com>
2022-09-15 21:21:12 -0400

    with all 3 bonus missions, italics for chosen seach field

--- src/main/java/org/launchcode/techjobs/mvc/controllers/SearchController.java
+++ src/main/java/org/launchcode/techjobs/mvc/controllers/SearchController.java
@@ -40,7 +40,7 @@ public class SearchController {
         }
         model.addAttribute("jobs", jobs);
         model.addAttribute("columns", columnChoices);
-
+        model.addAttribute("choice", searchType);
         return "search";
     }
 }
